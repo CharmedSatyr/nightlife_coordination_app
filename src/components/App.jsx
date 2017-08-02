724a7173a0b5b55e775f9ff2c632081567f921fc
CharmedSatyr <z@charmed.tech>
2017-08-02 22:33:50 +0900

    Updated logic to use React for all updates rather than vanilla/jQuery; removed transitions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,69 @@
 'use strict';
 
-import React from 'react';
-import commonFunctions from '../common/common.jsx';
-import $ from 'jquery';
+import React, {
+   Component
+} from 'react';
+
+import ajaxFunctions from '../common/ajax-functions.jsx';
 
 class App extends React.Component {
    constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+         city: 'New York',
+         venues: []
+      };
+      this.handleChange = this.handleChange.bind(this);
+   }
+   handleChange(event) {
+      this.setState({
+         city: event.currentTarget.value
+      });
+      this.pullVenues();
+   }
+   pullVenues() {
+      ajaxFunctions.ajaxRequest('POST', '/' + this.state.city, (response) => {
+
+         const venues = [];
+         JSON.parse(response)
+            .map((item) => {
+               console.log(item.name);
+               venues.push(item.name);
+            });
+
+         this.setState({
+            venues: venues
+         });
+      });
+   }
+   componentWillMount() {
+      this.pullVenues(this.state.city);
    }
    render() {
-
+      const children = [];
+      this.state.venues.map((item, index) => {
+         children.push(<Result key={index} item={item}/>);
+      });
 
       return (
          <div>
-
-          <p>Type your location below to see nightlife in your area.</p>
-          <input id='locationEntry' type='text'></input>
-          <button type='submit' onClick={commonFunctions.submitLocationEntry}>Submit</button>
-          <br/>
-         <div id='results'></div>
-
-       </div>
+          <h1>City: {this.state.city}</h1>
+            <input type='text' onChange={this.handleChange}/>
+            <br/>
+            Results:
+            <div>
+              {children}
+            </div>
+          </div>
       );
    };
 };
+
+const Result = ({
+   item
+}) => (
+   <div className='result'>{item}</div>
+);
+
 
 export default App;
